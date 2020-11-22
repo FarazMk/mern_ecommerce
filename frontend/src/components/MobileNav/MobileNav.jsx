fef278d5a6fbf16626cbd8c3ffbdfbbbb599b44f
amirmohammad_bashiri <amirmohamad.bashiri@gmail.com>
2020-11-22 14:11:37 +0330

    Mobile nav refactor

--- frontend/src/components/MobileNav/MobileNav.jsx
+++ frontend/src/components/MobileNav/MobileNav.jsx
@@ -4,44 +4,48 @@ import classes from "./mobileNav.module.css";
 const MobileNav = () => {
   return (
     <nav className={classes.nav}>
-      <Link type="button" className={classes.nav__link}>
+      <button type="button" className={classes.nav__link}>
         <i className="material-icons">menu</i>
-        <ul className={classes.menu}>
-          <div>
-            <li>
-              <Link to="">Women</Link>
-              <ul>
-                <li>
-                  <Link to="/women/skirt">Skirts</Link>
-                </li>
-                <li>
-                  <Link to="/women/dresses">Dresses</Link>
-                </li>
-                <li>
-                  <Link to="/women/knitwear">Knitwear</Link>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <Link to="#">Men</Link>
-              <ul>
-                <li>
-                  <Link to="/men/shirt">Shirts</Link>
-                </li>
-                <li>
-                  <Link to="/men/pants">Pants</Link>
-                </li>
-                <li>
-                  <Link to="/men/jacket">Jackets</Link>
-                </li>
-              </ul>
-            </li>
+        <div className={classes.dropdown}>
+          <div className="dropdown__wrapper__button">
+            <button type="button">Women</button>
+            <i className="material-icons">arrow_drop_up</i>
           </div>
-        </ul>
-      </Link>
-      <Link to="" className={classes.nav__link}>
+          <div className={classes.dropdown__links}>
+            <ul>
+              <li>
+                <Link to="/products/skirt">Skirt</Link>
+              </li>
+              <li>
+                <Link to="/products/dress">Dresses</Link>
+              </li>
+              <li>
+                <Link to="/products/knitewear">Knitewear</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="dropdown__wrapper__button">
+            <button type="button">Men</button>
+            <i className="material-icons">arrow_drop_up</i>
+          </div>
+          <div className={classes.dropdown__links}>
+            <ul>
+              <li>
+                <Link to="/products/shirt">Shirts</Link>
+              </li>
+              <li>
+                <Link to="/products/pants">Pants</Link>
+              </li>
+              <li>
+                <Link to="/products/jacket">Jackets</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </button>
+      <button type="button" className={classes.nav__link}>
         <i className="material-icons">search</i>
-      </Link>
+      </button>
       <NavLink
         to="/cart"
         className={classes.nav__link}
@@ -49,11 +53,7 @@ const MobileNav = () => {
       >
         <span className="material-icons">shopping_bag</span>
       </NavLink>
-      <Link
-        to="/profile"
-        className={classes.nav__link}
-        activeClassName={classes.active}
-      >
+      <Link to="/profile" className={classes.nav__link}>
         <span className="material-icons">person_outline</span>
       </Link>
     </nav>

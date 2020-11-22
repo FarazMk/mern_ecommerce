b44a80135cc5f6ddd0e1521f0d19da354ca2e01c
amirmohammad_bashiri <amirmohamad.bashiri@gmail.com>
2020-11-22 11:53:37 +0330

    Mobile nav menu setup

--- frontend/src/components/MobileNav/MobileNav.jsx
+++ frontend/src/components/MobileNav/MobileNav.jsx
@@ -4,37 +4,39 @@ import classes from "./mobileNav.module.css";
 const MobileNav = () => {
   return (
     <nav className={classes.nav}>
-      <Link to="#" className={classes.nav__link}>
+      <Link type="button" className={classes.nav__link}>
         <i className="material-icons">menu</i>
-        <ul>
-          <li>
-            <Link to="#">Women</Link>
-            <ul>
-              <li>
-                <Link to="/women/skirt">Skirts</Link>
-              </li>
-              <li>
-                <Link to="/women/dresses">Dresses</Link>
-              </li>
-              <li>
-                <Link to="/women/knitwear">Knitwear</Link>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <Link to="#">Men</Link>
-            <ul>
-              <li>
-                <Link to="/men/shirt">Shirts</Link>
-              </li>
-              <li>
-                <Link to="/men/pants">Pants</Link>
-              </li>
-              <li>
-                <Link to="/men/jacket">Jackets</Link>
-              </li>
-            </ul>
-          </li>
+        <ul className={classes.menu}>
+          <div>
+            <li>
+              <Link to="">Women</Link>
+              <ul>
+                <li>
+                  <Link to="/women/skirt">Skirts</Link>
+                </li>
+                <li>
+                  <Link to="/women/dresses">Dresses</Link>
+                </li>
+                <li>
+                  <Link to="/women/knitwear">Knitwear</Link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Link to="#">Men</Link>
+              <ul>
+                <li>
+                  <Link to="/men/shirt">Shirts</Link>
+                </li>
+                <li>
+                  <Link to="/men/pants">Pants</Link>
+                </li>
+                <li>
+                  <Link to="/men/jacket">Jackets</Link>
+                </li>
+              </ul>
+            </li>
+          </div>
         </ul>
       </Link>
       <Link to="" className={classes.nav__link}>
